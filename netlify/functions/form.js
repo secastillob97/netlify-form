fc98dea141dced051f27c6a6a5d0592440c96592
secastillob97 <se.castillob97@gmail.com>
2026-01-15 14:17:51 -0300

    Update form.js

--- netlify/functions/form.js
+++ netlify/functions/form.js
@@ -6,24 +6,22 @@ exports.handler = async (event) => {
     };
   }
 
-  // Shopify envía form-data, no JSON
   const params = new URLSearchParams(event.body);
 
   const name = params.get("name");
   const email = params.get("email");
+  const phone = params.get("phone");
+  const type = params.get("type");
   const message = params.get("message");
 
-  console.log("📩 Nuevo mensaje de contacto");
-  console.log("Nombre:", name);
-  console.log("Email:", email);
-  console.log("Mensaje:", message);
+  console.log("📩 Nuevo contacto");
+  console.log({ name, email, phone, type, message });
 
-return {
-  statusCode: 302,
-  headers: {
-    Location: "https://lasfloresdelareina.cl/pages/contacto-gracias",
-  },
-};
-    }),
+  return {
+    statusCode: 302,
+    headers: {
+      Location: "https://lasfloresdelareina.cl/pages/contacto-gracias",
+    },
   };
 };
+
